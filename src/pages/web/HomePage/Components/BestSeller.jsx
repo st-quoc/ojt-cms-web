d335973caf4383309e677e8a217fb5c63a335b4d
Doan Quoc Hoa <109152800+quochoa304@users.noreply.github.com>
2024-11-20 11:37:24 +0700

    feat[hoa-7]: update src

--- src/pages/web/HomePage/Components/BestSeller.jsx
+++ src/pages/web/HomePage/Components/BestSeller.jsx
@@ -1,4 +1,6 @@
+import { Title } from 'chart.js';
 import cailoaImage from '../../../../assets/cailoa.png';
+import { Product } from '../../../../component/Product';
 
 const products = [
   { id: 1, name: 'Historical', image: cailoaImage, items: 10 },
@@ -28,30 +30,11 @@ const products = [
 export const BestSeller = () => {
   return (
     <section id="bestseller" className="py-16 bg-white text-center">
-      <h2 className="text-2xl md:text-3xl font-bold mb-8">
-        MOST VINTAGE CATEGORY
-      </h2>
+      <Title title="MOST VINTAGE CATEGORY" />
+
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 px-4 md:px-8 max-w-6xl mx-auto">
         {products.map(product => (
-          <div
-            key={product.id}
-            className="p-6 bg-gray-50 border rounded-lg transform transition duration-300 hover:scale-105 shadow-md hover:shadow-lg"
-          >
-            <img
-              src={product.image}
-              alt={product.name}
-              className="mx-auto mb-4 w-24 h-24 sm:w-28 sm:h-28 md:w-32 md:h-32 object-cover"
-            />
-            <h3 className="text-lg sm:text-xl md:text-2xl font-semibold">
-              {product.name}
-            </h3>
-            <p className="text-gray-500 text-sm md:text-lg">
-              {product.items} items
-            </p>
-            <button className="border-2 border-slate-300 mt-4 px-4 py-2 rounded-lg bg-zinc-200 hover:bg-zinc-700 hover:text-gray-100 transition">
-              Add to cart
-            </button>
-          </div>
+          <Product key={product.id} product={product} />
         ))}
       </div>
     </section>
