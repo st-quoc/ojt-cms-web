1ebc6ac5dcb99ce1318da12c1effd355f3fc096a
Doan Quoc Hoa <quochoa29042001@gmail.com>
2024-12-04 09:52:47 +0700

    remove admin

--- src/pages/admin/Blogs/detail.jsx
+++
@@ -1,5 +0,0 @@
-import { Box } from '@mui/material';
-
-export const BlogDetailAdmin = () => {
-  return <Box className="p-4"></Box>;
-};
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes, Navigate } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import { HomePage } from '../pages/web/HomePage';
 import { LoginPage } from '../pages/login';
 import { ProductDetailPage } from '../pages/web/ProductDetail';
@@ -9,45 +9,8 @@ import { BlogPage } from '../pages/web/BlogPage';
 import { BlogDetail } from '../pages/web/BlogPage/BlogDetail';
 import { RegisterPage } from '../pages/register';
 import NoAccessPage from '../NoAccessPage';
-import { ProductCreateAdmin } from '../pages/admin/Products/create';
-import { ProductsListAdmin } from '../pages/admin/Products/list';
-import { DetailProductAdmin } from '../pages/admin/Products/detail';
-import { ProductEditAdmin } from '../pages/admin/Products/edit';
-import { BlogListAdmin } from '../pages/admin/Blogs/list';
-import { BlogCreateAdmin } from '../pages/admin/Blogs/create';
-import { BlogDetailAdmin } from '../pages/admin/Blogs/detail';
-import { BlogEditAdmin } from '../pages/admin/Blogs/edit';
-import { AdminLayout } from '../pages/admin';
-import { Dashboard } from '../pages/admin/Dashboard';
 import { AboutUs } from '../pages/web/AboutUs';
 import ProductsListPage from '../pages/web/ProductsListPage';
-import { VariantsPage } from '../pages/admin/Variants';
-
-const AdminLayoutProtectedRoute = ({ children }) => {
-  const userInfo = JSON.parse(localStorage.getItem('userInfo'));
-  const role = userInfo?.role;
-
-  if (!['admin', 'manager'].includes(role)) {
-    return <Navigate to="/login" replace />;
-  }
-
-  return children;
-};
-
-const ProtectedRoute = ({ children, requiredPermissions }) => {
-  const userInfo = JSON.parse(localStorage.getItem('userInfo'));
-  const permissions = userInfo?.permissions || [];
-
-  const hasPermission = requiredPermissions.every(permission =>
-    permissions.includes(permission),
-  );
-
-  if (!hasPermission) {
-    return <Navigate to="/no-access" replace />;
-  }
-
-  return children;
-};
 
 export const Router = () => {
   return (
@@ -63,87 +26,6 @@ export const Router = () => {
         <Route element={<BlogDetail />} path="blog/:blogId" />
         <Route element={<AboutUs />} path="AboutUs" />
       </Route>
-
-      <Route
-        path="admin"
-        element={
-          <AdminLayoutProtectedRoute>
-            <AdminLayout />
-          </AdminLayoutProtectedRoute>
-        }
-      >
-        <Route index element={<Dashboard />} />
-        <Route index element={<Dashboard />} path="dashboard" />
-
-        <Route
-          element={
-            <ProtectedRoute requiredPermissions={['create_product']}>
-              <ProductCreateAdmin />
-            </ProtectedRoute>
-          }
-          path="product/create"
-        />
-        <Route
-          element={
-            <ProtectedRoute requiredPermissions={['view_product']}>
-              <ProductsListAdmin />
-            </ProtectedRoute>
-          }
-          path="products"
-        />
-        <Route
-          element={
-            <ProtectedRoute requiredPermissions={['view_product']}>
-              <DetailProductAdmin />
-            </ProtectedRoute>
-          }
-          path="product/detail/:id"
-        />
-        <Route
-          element={
-            <ProtectedRoute requiredPermissions={['update_product']}>
-              <ProductEditAdmin />
-            </ProtectedRoute>
-          }
-          path="product/edit/:id"
-        />
-
-        <Route
-          element={
-            <ProtectedRoute requiredPermissions={['view_blog']}>
-              <BlogListAdmin />
-            </ProtectedRoute>
-          }
-          path="blogs"
-        />
-        <Route
-          element={
-            <ProtectedRoute requiredPermissions={['create_blog']}>
-              <BlogCreateAdmin />
-            </ProtectedRoute>
-          }
-          path="blog/create"
-        />
-        <Route element={<BlogDetailAdmin />} path="blog/detail/:blogID" />
-        <Route
-          element={
-            <ProtectedRoute requiredPermissions={['update_blog']}>
-              <BlogEditAdmin />
-            </ProtectedRoute>
-          }
-          path="blog/edit/:blogID"
-        />
-
-        <Route
-          element={
-            <ProtectedRoute requiredPermissions={['view_variants']}>
-              <VariantsPage />
-            </ProtectedRoute>
-          }
-          path="variants"
-        />
-      </Route>
-
       <Route element={<LoginPage />} path="login" />
       <Route element={<RegisterPage />} path="register" />
       <Route path="/no-access" element={<NoAccessPage />} />
